a693540892fabbab6fd7ecd0499f59df4ae61e5e
Vajidullah Khan <vajidullah_khan@ildc-inlp.idealake.com>
2025-08-11 21:31:37 +0530

    dummy video added

--- app/components/videoWrap/videoWrap.jsx
+++ app/components/videoWrap/videoWrap.jsx
@@ -4,7 +4,7 @@ const VideoWrap = () => {
   return (
     <div className="video-wrap">
         <div className="conatiner">
-        <video className="scroll-video" src="/assets/videos/on-page-scroll-text-overlapping.mp4" type="video/mp4" preload="auto"></video>
+        <video className="scroll-video" src="https://videos.pexels.com/video-files/2104648/2104648-hd_1280_720_30fps.mp4" type="video/mp4" preload="auto"></video>
 
         </div>
     </div>
